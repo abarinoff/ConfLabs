87ddf7fc1ab8756faa40b3492bb0b7bb4353a032
emabarinoff/em <emabarinoff@gmail.com/em>
2013-08-27 14:25:07 +0300

    - Fix for issue with running unit tests under Play 2.1.3.

--- project/Build.scala
+++ project/Build.scala
@@ -16,6 +16,16 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
+
+    // Fix for issue with running unit tests under Play 2.1.3
+    // more details: http://stackoverflow.com/questions/18275638/play-framework-2-1-3-doesnt-run-any-tests
+    testOptions in Test ~= { args =>
+      for {
+        arg <- args
+        val ta: Tests.Argument = arg.asInstanceOf[Tests.Argument]
+        val newArg = if(ta.framework == Some(TestFrameworks.JUnit)) ta.copy(args = List.empty[String]) else ta
+      } yield newArg
+    }
   )
 
 }
