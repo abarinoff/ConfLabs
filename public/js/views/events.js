f7e605269c765bcea46309b673cbf4862c65f858
j00ff <omostepaniuk@intelliarts.com>
2013-10-05 13:53:36 +0300

    Transferring changes to another machine

--- public/js/views/events.js
+++ public/js/views/events.js
@@ -15,8 +15,16 @@ function(_, Backbone, template) {
             // Bootstrap affix
             $(".events-sidebar").affix({
                 offset: {
-                    top: $("navbar").height() + 10,
-                    bottom: 41
+                    // position at which we switch to affix from affix-top (?)
+                    /*top: function() {
+                        var offsetTop = $(".events-sidebar").offset().top,
+                            sidebarMargin = parseInt($("#list-events").css('margin-top'), 10),
+                            navHeight = $("#navbar").height();
+
+                        return (this.top = offsetTop - navHeight - sidebarMargin)
+                    },*/
+                    top: 0,
+                    bottom: 81
                     /*bottom: function() {
                         return (this.bottom = $('footer').outerHeight(true))
                     }*/
